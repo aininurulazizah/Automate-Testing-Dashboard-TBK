3ddf570e5139bf0d89633161f7b2056bdb50d8d9
aininurulazizah <aini.nurul.tif421@polban.ac.id>
2026-09-07 16:16:38 +0700

    chore: update user profil & use first ordered test result by status

--- src/layouts/main-layout/topbar/AccountDropdown.tsx
+++ src/layouts/main-layout/topbar/AccountDropdown.tsx
@@ -89,7 +89,7 @@ const AccountDropdown = () => {
         <Box sx={{ display: { xs: 'none', xl: 'block' } }}>
           <Stack direction="row" alignItems="center" columnGap={6}>
             <Typography variant="h6" component="p" color="primary.darker" gutterBottom>
-              Musfiq
+              QC Tiketux
             </Typography>
             <IconifyIcon icon="ph:caret-down-bold" fontSize={16} color="primary.darker" />
           </Stack>
